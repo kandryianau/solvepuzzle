5df9ae0b99b2b4a4ba87ff489c4d0e98a104ac33
kandryianau <a_k_v@mail.ru>
2021-04-12 18:38:42 +0300

    Add puzzels objects file pauzzels.js

--- solution.js
+++ solution.js
@@ -1,17 +1,24 @@
-const puzzle = {
-  id: 8,
-  edges: {
-    top: { edgeTypeId: 8, type: 'inside' },
-    left: { edgeTypeId: 38, type: 'outside' },
-    right: null,
-    bottom: null,
-  },
-};
+const { puzzles } = require('./puzzles');
 
 function turnPuzzle(puzzle) {
-  const arrOfEdges = Object.entries(puzzle.edges);
-  //   arrOfEdges.push(arrOfEdges.shift());
-  console.log(arrOfEdges.shift());
+  const edgeKeys = Object.keys(puzzle.edges);
+  const edgeValues = Object.values(puzzle.edges);
+  edgeValues.unshift(edgeValues.pop());
+  puzzle.edges = Object.fromEntries(
+    edgeKeys.map((_, i) => [edgeKeys[i], edgeValues[i]])
+  );
+  return puzzle;
 }
 
-turnPuzzle(puzzle);
+function solvePuzzle(puzzles) {
+  const result = [];
+
+  result.push(puzzles[0]);
+  while (puzzles[0].edges.top || puzzles[0].edges.left) {
+    turnPuzzle(result[0]);
+  }
+
+  return result;
+}
+
+solvePuzzle(puzzles);
